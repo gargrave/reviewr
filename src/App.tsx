3330207680c865a4a92756794e761d8f6c3b6116
Gabe Hargrave <gabe.hargrave@vacasa.com>
2018-11-11 10:43:13 -0800

    Clean up initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import styles from './App.module.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.app}>
+        <h1>Reviewr</h1>
+      </div>
+    )
+  }
+}
+
+export default App
