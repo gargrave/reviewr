cf3def534163be7361c85f8e6f14ed5cc5e81eaa
Gabe Hargrave <gabe.hargrave@vacasa.com>
2018-11-21 19:27:53 -0800

    Fix Styleguidist config for TypeScript

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,12 +2,18 @@ const path = require('path');
 
 // Add the names of components we don't want to see in Styleguidist here
 // No paths or file extensions necessary; just the case-sensitive component name.
-const IGNORED_COMPONENTS = ['App', 'ReviewListPage'];
+const IGNORED_COMPONENTS = [
+  'App',
+  'ReviewForm',
+  'ReviewList',
+  'ReviewListPage',
+];
 const ignore = IGNORED_COMPONENTS.map(file => `src/app/**/${file}.tsx`);
 
 module.exports = {
   components: 'src/app/**/*.tsx',
-  ignore: ['src/**/*.ts', ...ignore],
+  ignore: ['src/**/*.ts', 'src/**/*.spec.{ts,tsx}', '**/*.d.ts', ...ignore],
+  propsParser: require('react-docgen-typescript').withDefaultConfig([{}]).parse,
   require: [
     path.join(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css'),
   ],
