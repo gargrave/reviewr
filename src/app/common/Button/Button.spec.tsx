46ac4c4f9d0753650f87745de8349c0bc4497a80
Gabe Hargrave <gabe.hargrave@vacasa.com>
2018-11-21 19:34:34 -0800

    Update Button component for linting and styleguidist

--- src/app/common/Button/Button.spec.tsx
+++ src/app/common/Button/Button.spec.tsx
@@ -1,20 +1,21 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
+import * as React from 'react'
+import { shallow } from 'enzyme'
 
-import Button, { ButtonNativeType, ButtonColor } from './Button';
+import Button, { ButtonNativeType, ButtonColor } from './Button'
 
 const defaultProps = {
   children: 'Hello',
   onClick: jest.fn(),
-};
+}
 
 describe('Button', () => {
   describe('Props', () => {
     it('sets default props correctly', () => {
-      const component = shallow(<Button {...defaultProps} />);
-      const props = component.props();
+      const component = shallow(<Button {...defaultProps} />)
+      const props = component.props()
 
-      expect(props.color).toBe(ButtonColor.Primary);
-    });
-  });
-});
+      expect(props.color).toBe(ButtonColor.Primary)
+      expect(props.type).toBe(ButtonNativeType.Button)
+    })
+  })
+})
